8b25d052bc0b9deb8d000a624b5e2f54038049c4
btingle <43322227+btingle@users.noreply.github.com>
2019-02-19 14:47:38 -0800

    Add files via upload
    I removed the polygondungeon assets and most of the rock assets bc of file size constraints. So the player is just a sphere instead of being a nice goblin or knight model.

---
+++ Assets/Scripts/Map/BridgeMeshGenerator.cs
@@ -0,0 +1,136 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using static MapUtils.MapConstants;
+
+public class BridgeMeshGenerator : MonoBehaviour
+{
+    private int[,] map;
+	private int width;
+	private int height;
+	private float cell_size;
+	private float wall_height;
+	
+	private List<Vector3> vertices;
+	private List<Vector2> uvs;
+	private List<int> triangles;
+	private Vector3 offset;
+	
+	private MeshFilter target_mesh;
+	
+	void set_config_variables()
+	{
+		MapConfiguration config = GameObject.FindGameObjectWithTag("Map").GetComponent<MapConfiguration>();
+		this.cell_size = config.cell_size;
+		this.wall_height = config.wall_height;
+		this.target_mesh = GetComponent<MeshFilter>();
+	}
+	
+	public void generate_bridge_mesh(int[,] map, Vector3 offset)
+	{
+		set_config_variables();
+		this.map = map;
+		this.width = map.GetLength(0);
+		this.height = map.GetLength(1);
+		this.offset = offset;
+		this.offset.y -= 0.005f;
+		
+		vertices = new List<Vector3>();
+		uvs = new List<Vector2>();
+		triangles = new List<int>();
+		
+		triangulate_all_bridges();
+		
+		Mesh mesh = target_mesh.mesh;
+		
+		mesh.triangles = null;
+		mesh.vertices = vertices.ToArray();
+		mesh.uv = uvs.ToArray();
+		mesh.triangles = triangles.ToArray();
+		mesh.RecalculateNormals();
+	}
+	
+	void triangulate_all_bridges()
+	{
+		for (int x = 0; x < width; x++) {
+			for (int y = 0; y < height; y++) {
+				if (map[x, y] == BRIDGE) {
+					triangulate_bridge(x, y);
+				}
+			}
+		}
+	}
+	
+	void triangulate_bridge(int x, int y) 
+	{
+		triangulate_platform(x, y, cell_size / 4);
+		
+		triangulate_leg((x * cell_size + 0.0625f), (y * cell_size + 0.0625f), 0.0625f, wall_height);
+		triangulate_leg((x * cell_size + 0.9375f), (y * cell_size + 0.0625f), 0.0625f, wall_height);
+		triangulate_leg((x * cell_size + 0.0625f), (y * cell_size + 0.9375f), 0.0625f, wall_height);
+		triangulate_leg((x * cell_size + 0.9375f), (y * cell_size + 0.9375f), 0.0625f, wall_height);
+	}
+	
+	void triangulate_platform(float x, float y, float height)
+	{
+		triangle_fan(
+			new Vector3[] {
+				new Vector3((x + 0), 0f, (y + 0) ), new Vector3((x + 0), 0f, (y + 1) ),
+				new Vector3((x + 1), 0f, (y + 1) ), new Vector3((x + 1), 0f, (y + 0) ) },
+			new Vector2[] {
+				new Vector2(0f, 0f), new Vector2(1f, 0f), new Vector2(1f, height), new Vector2(0f, height) });
+		triangulate_leg((x + (cell_size / 2f)), y + (cell_size / 2f), cell_size / 2f, height);
+	}
+	
+	void triangulate_leg(float x, float y, float thickness, float height)
+	{
+		triangle_fan(
+			new Vector3[] {
+				new Vector3((x - thickness), 0f			 , (y - thickness)), new Vector3((x + thickness), 0f		  , (y - thickness)),
+				new Vector3((x + thickness), -height	 , (y - thickness)), new Vector3((x - thickness), -height	  , (y - thickness)) },
+			new Vector2[] {
+				new Vector2(0f, 0f), new Vector2(1f, 0f), new Vector2(1f, height), new Vector2(0f, height) });
+		triangle_fan(
+			new Vector3[] {
+				new Vector3((x + thickness), 0f			 , (y - thickness)), new Vector3((x + thickness), 0f		  , (y + thickness)),
+				new Vector3((x + thickness), -height	 , (y + thickness)), new Vector3((x + thickness), -height	  , (y - thickness)) },
+			new Vector2[] {
+				new Vector2(0f, 0f), new Vector2(1f, 0f), new Vector2(1f, height), new Vector2(0f, height) });
+		triangle_fan(
+			new Vector3[] {
+				new Vector3((x + thickness), 0f			 , (y + thickness)), new Vector3((x - thickness), 0f		  , (y + thickness)),
+				new Vector3((x - thickness), -height	 , (y + thickness)), new Vector3((x + thickness), -height	  , (y + thickness)) },
+			new Vector2[] {
+				new Vector2(0f, 0f), new Vector2(1f, 0f), new Vector2(1f, height), new Vector2(0f, height) });
+		triangle_fan(
+			new Vector3[] {
+				new Vector3((x - thickness), 0f			 , (y + thickness)), new Vector3((x - thickness), 0f		  , (y - thickness)),
+				new Vector3((x - thickness), -height	 , (y - thickness)), new Vector3((x - thickness), -height	  , (y + thickness)) },
+			new Vector2[] {
+				new Vector2(0f, 0f), new Vector2(1f, 0f), new Vector2(1f, height), new Vector2(0f, height) });
+	}
+	
+	void triangle_fan(Vector3[] verts, Vector2[] uv)
+	{
+		write_triangles((verts.Length - 2) * 3);
+		for (int i = 0; i < verts.Length - 2; i ++) {
+			vertices.Add(verts[0] - offset);
+			vertices.Add(verts[i + 1] - offset);
+			vertices.Add(verts[i + 2] - offset);
+		}
+		for (int i = 0; i < uv.Length - 2; i++) {
+			uvs.Add(uv[0]);
+			uvs.Add(uv[i + 1]);
+			uvs.Add(uv[i + 2]);
+		}
+	}
+	
+	void write_triangles(int amt)
+	{
+		for (int i = 0; i < amt; i++) {
+			triangles.Add(vertices.Count + i);
+		}
+	}
+	
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using MapUtils;
+using static MapUtils.MapConstants;
+
+public class Player : MonoBehaviour
+{
+	private MapGenerator map_instance; // reference to MapGenerator instance with map data
+	private TileSelector tile_selector; // reference to map tile selector
+	
+	private Pos grid_pos;
+	private bool moving = false;
+	
+    // Gets references to necessary game components
+    void Start()
+    {
+        tile_selector = GameObject.FindGameObjectWithTag("Map").transform.Find("TileSelector").GetComponent<TileSelector>();
+		map_instance = GameObject.FindGameObjectWithTag("Map").GetComponent<MapGenerator>();
+    }
+
+	// if right mouse button is pressed, move player model to hover position
+	// if hover position is on a bridge tile, change the player model
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1) && map_instance.map != null && !moving) {
+			
+			grid_pos = tile_selector.grid_position;
+			StartCoroutine(smooth_movement(transform.position, tile_selector.hover_position));
+		}
+    }
+	
+	IEnumerator smooth_movement(Vector3 origin, Vector3 target)
+	{
+		float speed = 30f;
+		float distance_to_target = Vector3.Distance(origin, target);
+		float time = 0f;
+		
+		moving = true;
+		transform.LookAt(target);
+		
+		while(time < Mathf.PI / 2f) {
+			
+			time += (Time.deltaTime * speed) / distance_to_target;
+			transform.position = Vector3.Lerp(origin, target, Mathf.Sin(time));
+			yield return null;
+		}
+		
+		transform.position = target;
+		moving = false;
+	}
+}
