80fe4b6650d3e0a635552cf6c56e0ec27a7d7fc2
btingle <43322227+btingle@users.noreply.github.com>
2019-02-22 16:24:49 -0800

    Add files via upload
    Added pathfinding script (Navigation.cs)

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,47 +7,104 @@ using static MapUtils.MapConstants;
 
 public class Player : MonoBehaviour
 {
+	private NavigationHandler nav_map;
 	private MapGenerator map_instance; // reference to MapGenerator instance with map data
 	private TileSelector tile_selector; // reference to map tile selector
 	
 	private Pos grid_pos;
+	private static Pos INVALID = new Pos(-1, -1);
 	private bool moving = false;
+	private int current_map_iteration = -1;
+	private int width;
+	private int height;
+	
+	private Pos[] path;
+	public int move_budget;
+	public float speed;
 	
     // Gets references to necessary game components
     void Start()
     {
         tile_selector = GameObject.FindGameObjectWithTag("Map").transform.Find("TileSelector").GetComponent<TileSelector>();
 		map_instance = GameObject.FindGameObjectWithTag("Map").GetComponent<MapGenerator>();
+		set_config_variables();
+		grid_pos = new Pos(-1, -1);
+		nav_map = null;
     }
+	
+	void set_config_variables()
+	{
+		MapConfiguration config = GameObject.FindGameObjectWithTag("Map").GetComponent<MapConfiguration>();
+		width = config.width;
+		height = config.height;
+	}
 
 	// if right mouse button is pressed, move player model to hover position
 	// if hover position is on a bridge tile, change the player model
     void Update()
     {
-        if (Input.GetMouseButtonDown(1) && map_instance.map != null && !moving) {
+        if (map_instance.map != null) {
 			
-			grid_pos = tile_selector.grid_position;
-			StartCoroutine(smooth_movement(transform.position, tile_selector.hover_position));
+			if (grid_pos == INVALID) {
+				place_randomly_on_map();
+			}
+			
+			if (current_map_iteration != map_instance.iteration) {
+				
+				current_map_iteration = map_instance.iteration;
+				nav_map = new NavigationHandler(map_instance.map);
+				place_randomly_on_map();
+				set_config_variables();
+				transform.position = map_instance.grid_to_world(grid_pos);
+			}
+			
+			else if (Input.GetMouseButtonDown(1) && !moving) {
+				
+				Stack<Pos> path = nav_map.find_shortest_path(grid_pos, tile_selector.grid_position);
+				if (path != null) {
+					StartCoroutine(smooth_movement(path));
+					grid_pos = tile_selector.grid_position;
+				}
+				
+				transform.position = map_instance.grid_to_world(tile_selector.grid_position);
+				
+			}
 		}
     }
 	
-	IEnumerator smooth_movement(Vector3 origin, Vector3 target)
+	void place_randomly_on_map()
 	{
-		float speed = 30f;
-		float distance_to_target = Vector3.Distance(origin, target);
-		float time = 0f;
+		System.Random rng = new System.Random(0);
 		
-		moving = true;
-		transform.LookAt(target);
+		int x = rng.Next(0, width - 1);
+		int y = rng.Next(0, height - 1);
 		
-		while(time < Mathf.PI / 2f) {
-			
-			time += (Time.deltaTime * speed) / distance_to_target;
-			transform.position = Vector3.Lerp(origin, target, Mathf.Sin(time));
-			yield return null;
+		while (map_instance.map[x, y] == EMPTY || map_instance.map[x, y] == EDGE) {
+			x = rng.Next(0, width - 1);
+			y = rng.Next(0, height - 1);
 		}
 		
-		transform.position = target;
+		grid_pos = new Pos(x, y);
+	}
+	
+	IEnumerator smooth_movement(Stack<Pos> path)
+	{
+		moving = true;
+		
+		Vector3 origin, target;
+		while (path.Count > 0) {
+			origin = transform.position;
+			target = map_instance.grid_to_world(path.Pop());
+			float dist = Vector3.Distance(origin, target);
+			float time = 0f;
+			while(time < 1f && dist > 0f) {
+				
+				time += (Time.deltaTime * speed) / dist;
+				transform.position = Vector3.Lerp(origin, target, time);
+				yield return null;
+			}
+		}
+		
 		moving = false;
 	}
 }
