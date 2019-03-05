cfb58316ae49706d8c2bd904d333bd47385aaf76
krozet <keawa.rozet@yahoo.com>
2019-03-05 14:12:51 -0800

    added basic enemyGroup, enemyGroupManager, and EnemySpawnProperty.

---
+++ Assets/Scripts/EnemyGroupManager.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyGroupManager : MonoBehaviour
+{
+    [Header("Enemy Group Settings")]
+    public int maxNumberOfEnemyGroups;
+    public int minNumberOfEnemyGroups;
+}
