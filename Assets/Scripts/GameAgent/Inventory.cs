833fccd30c1f7a06532487592b6744604e359b6f
Diana Luu <Diana@sfs-wifi-dhcp-10-143-153-148.wifi.sfsu.edu>
2019-03-12 15:26:04 -0700

    adding comments for code

--- Assets/Scripts/GameAgent/Inventory.cs
+++ Assets/Scripts/GameAgent/Inventory.cs
@@ -9,6 +9,9 @@ public class Inventory : MonoBehaviour
     public Item[] items = new Item[numItemSlots];
     public const int numItemSlots = 4;
 
+    /*
+     * Adds an item to the items array. Returns true if success, false if fail  
+     */  
     public bool AddItem(Item addItem)
     {
         for (int i = 0; i < items.Length; i++)
@@ -22,6 +25,14 @@ public class Inventory : MonoBehaviour
         return false;
     }
 
+    /*
+     * TODO: create a method for adding more of one item? but Item class already
+     * comes with amount field, so maybe not needed   
+     */
+
+    /*
+     * Removes an item from the items array. Returns true if success, false if fail  
+     */  
     public bool RemoveItem(Item removeItem)
     {
         for (int i = 0; i < items.Length; i++)
@@ -29,6 +40,29 @@ public class Inventory : MonoBehaviour
             if (items[i] == removeItem)
             {
                 items[i] = null;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /*
+     *   Removes a designated amount of an item. Returns true if success, false
+     * if fail.
+     *   NOTE: Doesn't account for if attempting to remove more than amount of 
+     * item player has yet. Need to handle this case somehow.
+     */  
+    public bool RemoveAmtofItem(Item removeItem, int amount)
+    {
+        for (int i = 0; i < items.Length; i++)
+        {
+            if (items[i].ID == removeItem.ID)
+            {
+                items[i].Amount -= amount;
+                if (items[i].Amount <= 0)
+                {
+                    items[i] = null;
+                }
                 return true;
             }
         }
--- Assets/Scripts/GameAgent/Item.cs
+++ Assets/Scripts/GameAgent/Item.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public abstract class Item
 {
-    public int maxAmount;
-    public string Name;
-    public int ID;
-    public int Amount;
+    //derive item types from this class
+    public int maxAmount; //max amount of item that can be held
+    public string Name; //name of the item
+    public int ID; //unique ID used to identity item in code (if necessary)
+    public int Amount; //current amount of item held
 }
