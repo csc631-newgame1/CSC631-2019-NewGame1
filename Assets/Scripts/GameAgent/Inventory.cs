b3182b407ddabb3033f976917c63b150ee8c9534
Diana Luu <Diana@sfs-wifi-dhcp-10-143-153-148.wifi.sfsu.edu>
2019-03-12 15:11:58 -0700

    adding base code for inventory

---
+++ Assets/Scripts/GameAgent/Inventory.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Inventory : MonoBehaviour
+{
+
+    public Item[] items = new Item[numItemSlots];
+    public const int numItemSlots = 4;
+
+    public bool AddItem(Item addItem)
+    {
+        for (int i = 0; i < items.Length; i++)
+        {
+            if (items[i] == null)
+            {
+                items[i] = addItem;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public bool RemoveItem(Item removeItem)
+    {
+        for (int i = 0; i < items.Length; i++)
+        {
+            if (items[i] == removeItem)
+            {
+                items[i] = null;
+                return true;
+            }
+        }
+        return false;
+    }
+}
