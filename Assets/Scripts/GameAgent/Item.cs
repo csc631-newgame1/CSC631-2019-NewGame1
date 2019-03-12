16e0d08890dc27ba86680459baa0c711a170c3b1
Diana Luu <Diana@sfs-wifi-dhcp-10-143-153-148.wifi.sfsu.edu>
2019-03-12 15:11:58 -0700

    adding base code for inventory

---
+++ Assets/Scripts/GameAgent/Item.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Item
+{
+    public int maxAmount;
+    public string Name;
+    public int ID;
+    public int Amount;
+}
