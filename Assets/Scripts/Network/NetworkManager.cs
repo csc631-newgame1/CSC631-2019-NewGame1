95a1fb65e5964904aed4dd262cf03206950590ad
Ben Tingle <ben@tingle.org>
2019-03-20 12:46:51 -0700

    Added Network Support
    Heed this description-
    I've hardcoded in the server IP, and it should be always running, but if you run into issues with the server
    you can use Network.disable(), and any commands submitted will be immediately piped back into the system, making it
    effectively a single-player game. Network commands don't do anything yet, but if you check NetworkManager, there is
    already a skeleton in place for handling a variety of incoming commands.

---
+++ Assets/Scripts/Network/NetworkManager.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using MapUtils;
+
+public class NetworkManager : MonoBehaviour
+{
+	public int clientID = 0;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        Network.connectToServer("18.223.24.205", 1337);
+		Network.submitCommand(new MoveCommand(new Pos(2, 2), new Pos(3, 3)));
+		Network.submitCommand(new StartCommand());
+		Network.submitCommand(new EndCommand());
+		Network.submitCommand(new NicknameCommand("Big Boy", clientID));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        List<NetworkCommand> cmds = Network.getPendingCommands();
+		
+		if (cmds != null) {
+			foreach (NetworkCommand cmd in cmds) {
+				
+				if (cmd is MoveCommand) {
+					MoveCommand move = (MoveCommand) cmd;
+					Debug.Log("Move received! From " + move.a + " to " + move.b);
+				}
+				if (cmd is ReadyCommand) {
+					ReadyCommand ready = (ReadyCommand) cmd;
+					Debug.Log("Received READY");
+				}
+				if (cmd is NicknameCommand) {
+					NicknameCommand nickname = (NicknameCommand) cmd;
+					Debug.Log("Received nickname: " + nickname.nickname);
+				}
+				if (cmd is JoinCommand) {
+					JoinCommand _join = (JoinCommand) cmd; // to avoid conflict with 'join' keyword
+					if (clientID == 0) {
+						clientID = _join.clientID;
+						Debug.Log("Joined game! Client ID #" + clientID + " assigned!");
+					} else {
+						Debug.Log("New player with client ID #" + _join.clientID + " joined!");
+					}
+				}
+				if (cmd is StartCommand) {
+					StartCommand start = (StartCommand) cmd;
+					Debug.Log("Received START");
+				}
+				if (cmd is EndCommand) {
+					EndCommand end = (EndCommand) cmd;
+					Debug.Log("Received END");
+				}
+				if (cmd is DisconnectCommand) {
+					DisconnectCommand disconnect = (DisconnectCommand) cmd;
+					Debug.Log("Received DISCONNECT from cient #" + disconnect.clientID);
+				}
+			}
+		}
+    }
+	
+	void OnApplicationQuit()
+	{
+		Network.disconnectFromServer();
+	}
+}
