c5737ca6cbc30b50734244177ae99beb0843c865
krozet <keawa.rozet@yahoo.com>
2019-03-21 19:32:10 -0700

    player now looks at the target it's attacking.

--- Assets/Scripts/Map/MapManager.cs
+++ Assets/Scripts/Map/MapManager.cs
@@ -64,7 +64,7 @@ public class MapManager : MonoBehaviour
 		GameAgent agent = clone.GetComponent<GameAgent>();
 
         if (stats == null) {
-            agent.init_agent(pos, new GameAgentStats(CharacterClassOptions.Knight, 1));
+            agent.init_agent(pos, new GameAgentStats(CharacterClassOptions.Mage, 1));
         } else {
             agent.init_agent(pos, stats);
         }
