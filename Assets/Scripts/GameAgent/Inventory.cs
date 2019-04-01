5c7845dbf58e7f4e6240bb9a6dbe689bbb0ef67a
Diana Luu <Diana@Jamess-MacBook-Pro.local>
2019-03-31 19:59:35 -0700

    hotfix: removing monobehavior from inventory

--- Assets/Scripts/GameAgent/Inventory.cs
+++ Assets/Scripts/GameAgent/Inventory.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Inventory : MonoBehaviour
+public class Inventory
 {
 
     public Item[] items = new Item[numItemSlots];
