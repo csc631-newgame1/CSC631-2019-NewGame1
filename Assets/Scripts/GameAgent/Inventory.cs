e3ad2cd5beadf2a2869b0084274f9841dbaed980
Diana Luu <Diana@Jamess-MacBook-Pro.local>
2019-03-31 17:08:50 -0700

    streamline inventory.cs and add inventory to gameagent

--- Assets/Scripts/GameAgent/Inventory.cs
+++ Assets/Scripts/GameAgent/Inventory.cs
@@ -9,63 +9,49 @@ public class Inventory : MonoBehaviour
     public Item[] items = new Item[numItemSlots];
     public const int numItemSlots = 4;
 
-    /*
-     * Adds an item to the items array. Returns true if success, false if fail  
-     */  
-    public bool AddItem(Item addItem)
+    //used for adding items to the inventory
+    //item MUST have amount specified
+    public void AddItemToSlot(int slot, Item item)
     {
-        for (int i = 0; i < items.Length; i++)
+        if (slot >= numItemSlots)
         {
-            if (items[i] == null)
-            {
-                items[i] = addItem;
-                return true;
-            }
+            return; //out of bounds
         }
-        return false;
+        items[slot] = item;
     }
 
-    /*
-     * TODO: create a method for adding more of one item? but Item class already
-     * comes with amount field, so maybe not needed   
-     */
-
-    /*
-     * Removes an item from the items array. Returns true if success, false if fail  
-     */  
-    public bool RemoveItem(Item removeItem)
+    //used to remove item completely from inventory
+    //used for throwing away items (not using them)
+    public void RemoveItemFromSlot(int slot)
     {
-        for (int i = 0; i < items.Length; i++)
+        if (slot >= numItemSlots)
         {
-            if (items[i] == removeItem)
-            {
-                items[i] = null;
-                return true;
-            }
+            return; //out of bounds
         }
-        return false;
+        items[slot] = null;
     }
 
-    /*
-     *   Removes a designated amount of an item. Returns true if success, false
-     * if fail.
-     *   NOTE: Doesn't account for if attempting to remove more than amount of 
-     * item player has yet. Need to handle this case somehow.
-     */  
-    public bool RemoveAmtofItem(Item removeItem, int amount)
+    //returns item in slot
+    public Item GetItemFromSlot(int slot)
     {
-        for (int i = 0; i < items.Length; i++)
+        if (slot >= numItemSlots)
         {
-            if (items[i].ID == removeItem.ID)
-            {
-                items[i].Amount -= amount;
-                if (items[i].Amount <= 0)
-                {
-                    items[i] = null;
-                }
-                return true;
-            }
+            return null; //out of bounds
         }
-        return false;
+        return items[slot];
+    }
+
+    //decreases amount of item by 1
+    //used when item is being used, i.e. potion consumed
+    public void DecrementItemAtSlot(int slot)
+    {
+        if (slot >= numItemSlots)
+        {
+            return; //out of bounds
+        }
+        if ((items[slot].Amount--) <= 0)
+        {
+            items[slot] = null;
+        }
     }
 }
