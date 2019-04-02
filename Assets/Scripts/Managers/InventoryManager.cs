2e8f67195dd89f2d320db7d9e0205e267d7ec5f1
Diana Luu <Diana@sfs-wifi-dhcp-10-143-156-70.wifi.sfsu.edu>
2019-04-02 16:43:52 -0700

    add item as param to use item delegate

--- Assets/Scripts/Managers/InventoryManager.cs
+++ Assets/Scripts/Managers/InventoryManager.cs
@@ -14,7 +14,7 @@ public class InventoryManager : MonoBehaviour
 
     //define delegate to encapsulate item functions to store in dictionary
     //item functions will take in game agent to apply item effects to that agent
-    delegate void ItemFunc(GameAgent agent);
+    delegate void ItemFunc(Item item, GameAgent agent);
     IDictionary<int, ItemFunc> itemFuncs = new Dictionary<int, ItemFunc>();
 
     public static InventoryManager instance = null;
@@ -29,20 +29,54 @@ public class InventoryManager : MonoBehaviour
         itemFuncs.Add(2, ApplyManaPotion);
     }
 
-    public void UseItem(int itemID, GameAgent agent)
+    public void UseItem(Item item, GameAgent agent)
     {
-        //uses itemID to know which item func to call, will pass game agent 
-        itemFuncs[itemID](agent);
+        //uses item ID to know which item func to call
+        //passes item and game agent (for use in equipment)
+        itemFuncs[item.ID](item, agent);
     }
 
-    private void ApplyHealthPotion(GameAgent agent)
+    private void ApplyHealthPotion(Item item, GameAgent agent)
     {
         //decrement item in player inventory, call HP increase func (if applicable)
         Debug.Log("Applying HP potion!");
     }
 
-    private void ApplyManaPotion(GameAgent agent)
+    private void ApplyManaPotion(Item item, GameAgent agent)
     {
         Debug.Log("Applying MP potion!");
+    }
+
+
+    //FOLLOWING METHODS FOR EQUIPMENT SYSTEM
+
+    private void EquipHelmet(Item item, GameAgent agent)
+    {
+        Debug.Log("Equipping helmet!");
+    }
+
+    private void EquipArmor(Item item, GameAgent agent)
+    {
+        Debug.Log("Equipping armor!");
+    }
+
+    private void EquipWeapon(Item item, GameAgent agent)
+    {
+        Debug.Log("Equipping weapon!");
+    }
+
+    private void EquipGloves(Item item, GameAgent agent)
+    {
+        Debug.Log("Equipping gloves!");
+    }
+
+    private void EquipShoes(Item item, GameAgent agent)
+    {
+        Debug.Log("Equipping shoes!");
+    }
+
+    private void EquipOffhand(Item item, GameAgent agent)
+    {
+        Debug.Log("Equipping offhand item!");
     }
 }
