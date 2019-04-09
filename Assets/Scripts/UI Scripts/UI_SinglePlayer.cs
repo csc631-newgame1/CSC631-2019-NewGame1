8b83a3de6d9ddd6ed9acac935dcd794f324ca3ed
Ben Tingle <ben@tingle.org>
2019-04-09 14:42:34 -0700

    New lobby menu & things

---
+++ Assets/Scripts/UI Scripts/UI_SinglePlayer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_SinglePlayer : MonoBehaviour
+{
+    private GameObject Panel_Main;
+	
+	public string characterClass { get; set;} 
+	
+	public void Awake()
+	{
+		Panel_Main = transform.parent.Find("MainMenu").gameObject;
+	}
+	
+	public void Start()
+	{
+		gameObject.SetActive(false);
+	}
+	
+    public void StartGame()
+	{
+		
+	}
+	
+	public void Back()
+	{
+		gameObject.SetActive(false);
+		Panel_Main.SetActive(true);
+	}
+}
