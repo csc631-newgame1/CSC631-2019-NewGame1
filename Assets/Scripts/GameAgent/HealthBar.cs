a7317127d6427170b8050edcc6bca15262a28406
Diana Luu <Diana@Jamess-MacBook-Pro.local>
2019-04-10 23:58:51 -0700

    add audio to menu and procedural

--- Assets/Scripts/GameAgent/HealthBar.cs
+++ Assets/Scripts/GameAgent/HealthBar.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class HealthBar : MonoBehaviour
 {
+    public GameObject EnemyHealthBar;
+    Transform target;
+
     // Start is called before the first frame update
     void Start()
     {
