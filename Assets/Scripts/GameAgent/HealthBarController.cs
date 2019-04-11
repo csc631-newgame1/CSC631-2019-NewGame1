3a6f469e5f7527236ffa954179fa6bca2cb86dea
Diana Luu <Diana@sfs-wifi-dhcp-10-143-156-90.wifi.sfsu.edu>
2019-04-11 14:57:53 -0700

    add in-game hp bar for game agents

---
+++ Assets/Scripts/GameAgent/HealthBarController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthBarController : MonoBehaviour
+{
+    public GameObject healthbar;
+    HealthBar bar;
+    private static GameObject canvas;
+    GameObject instance;
+    GameAgent parent;
+    Vector3 offset = new Vector3(0, 20);
+
+    void Start()
+    {
+        canvas = GameObject.Find("UICanvas"); //get canvas ref
+
+        instance = Instantiate(healthbar); //instantiate prefab
+        Vector2 screenPos = Camera.main.WorldToScreenPoint(gameObject.transform.position);
+        parent = GetComponent<GameAgent>();
+        bar = instance.GetComponentInChildren<HealthBar>();
+
+        instance.transform.SetParent(canvas.transform, false); //set canvas as parent
+        instance.transform.position = screenPos;
+
+        bar.SetSliderValue(1);
+    }
+
+    void Update()
+    {
+        bar.SetSliderValue(parent.stats.currentHealth / parent.stats.maxHealth);
+        Vector3 wantedPosition = Camera.main.WorldToScreenPoint(gameObject.transform.position) + offset;
+        instance.transform.position = wantedPosition;
+    }
+}
