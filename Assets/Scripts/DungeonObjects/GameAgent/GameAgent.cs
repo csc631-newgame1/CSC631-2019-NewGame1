b0d841d76e0acf9077d9d1dfc43c7bc15f7e33cc
Ben Tingle <ben@tingle.org>
2019-04-22 11:41:20 -0700

    gameagent change

--- Assets/Scripts/DungeonObjects/GameAgent/GameAgent.cs
+++ Assets/Scripts/DungeonObjects/GameAgent/GameAgent.cs
@@ -11,7 +11,7 @@ public abstract class GameAgent : DungeonObject
     public float speed;
 	public string nickname;
     public GameAgentStats stats;
-    public GameAgentAction currentAction;
+    //public GameAgentAction currentAction;
 	
     public GameAgentState currentState;
 	public AIComponent AI;
